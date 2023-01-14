ff6c4458438370404d7e37a4ab1c7c658c3ed651
DevAntonioRogers <hydrohs6@gmail.com>
2023-01-13 21:44:01 -0500

    Bug Fixes

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,20 +1,24 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 
 const CampsiteCard = ({ campsite }) => {
-  const { image, name } = campsite
+  const { id, image, name } = campsite
   return (
-    <Card>
-      <CardImg
-        width='100%'
-        src={image}
-        alt={name}
-      />
+    <Link to={`${id}`}>
+      <Card>
+        <CardImg
+          width='100%'
+          src={image}
+          alt={name}
+        />
 
-      <CardImgOverlay>
-        <CardTitle>{name}</CardTitle>
-      </CardImgOverlay>
-    </Card>
+        <CardImgOverlay>
+          <CardTitle>{name}</CardTitle>
+        </CardImgOverlay>
+      </Card>
+    </Link>
   );
 }
 
