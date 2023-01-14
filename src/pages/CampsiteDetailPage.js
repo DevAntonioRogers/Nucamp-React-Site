72e570a3cbb3e6e199a4a29d372c2e3e9444ba91
DevAntonioRogers <hydrohs6@gmail.com>
2023-01-13 22:24:24 -0500

    Added Comments

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -2,6 +2,9 @@ import { Container, Row } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import { selectCampsiteById } from '../features/campsites/campsitesSlice';
 import CampsiteDetail from '../features/campsites/CampsiteDetail';
+import CommentsList from '../features/comments/CommentList';
+
+
 
 const CampsiteDetailPage = () => {
   const { campsiteId } = useParams();
@@ -11,6 +14,7 @@ const CampsiteDetailPage = () => {
     <Container>
       <Row>
         <CampsiteDetail campsite={campsite} />
+        <CommentsList campsiteId={campsiteId} />
       </Row>
     </Container>
   );
