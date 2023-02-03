e15cb534cd49a93a37f23ee647d2a069aad4320e
DevAntonioRogers <hydrohs6@gmail.com>
2023-02-02 20:46:39 -0500

    Fetching Data

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Header />
