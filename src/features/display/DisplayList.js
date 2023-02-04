3ab1b6724a6ce91df60f4cdb8a7057e98db8ba23
DevAntonioRogers <hydrohs6@gmail.com>
2023-02-03 20:23:27 -0500

    Added loading and error components

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -4,6 +4,8 @@ import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
 import { useSelector } from 'react-redux';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
 
 const DisplayList = () => {
   const items = useSelector((state) => [
@@ -15,10 +17,17 @@ const DisplayList = () => {
   return (
     <Row>
       {items.map((item, idx) => {
+        const { featuredItem, isLoading, errMsg } = item;
+        if (isLoading) {
+          return <Loading key={idx} />;
+        }
+        if (errMsg) {
+          return <Error errMsg={errMsg} key={idx} />;
+        }
         return (
-          item && (
+          featuredItem && (
             <Col md className='m-1' key={idx}>
-              <AnimatedDisplayCard item={item} />
+              <AnimatedDisplayCard item={featuredItem} />
             </Col>
           )
         );
